ce30f299dd2dd3466ad51d2f18f7010a06c12109
Andrey-Sabile <sabileandrey@gmail.com>
2025-10-15 20:48:14 +0800

    Expanded methods for Guardian and Student entities.

--- src/Domain/Entities/Guardian.cs
+++ src/Domain/Entities/Guardian.cs
@@ -10,4 +10,59 @@ public class Guardian : BaseAuditableEntity
     public int PhoneNumber { get; private set; }
     public IList<Student> Students { get; private set; } = [];
     public Address Address { get; private set; } = null!;
+
+    public static Guardian Create(
+    string firstName,
+    string lastName,
+    string email,
+    int phoneNumber,
+    IList<Student> students,
+    Address address
+    )
+    {
+        var guardian = new Guardian();
+        guardian.SetDetails(firstName, lastName, email, phoneNumber, students, address);
+        return guardian;
+    }
+
+    private void SetDetails(
+        string firstName,
+        string lastName,
+        string email,
+        int phoneNumber,
+        IList<Student> students,
+        Address address
+    )
+    {
+        ValidateFields(firstName, lastName, email, phoneNumber, students, address);
+
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Students = students;
+        Address = address;
+    }
+
+    private static void ValidateFields(string firstName, string lastName, string email, int phoneNumber, IList<Student> students, Address address)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException("FirstName is required.", nameof(firstName));
+
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException("LastName is required.", nameof(lastName));
+
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("email is required.", nameof(email));
+
+        ArgumentNullException.ThrowIfNull(address);
+
+        ArgumentNullException.ThrowIfNull(students);
+    }
+
+    // Soft Delete
+
+    // Guard against removal of final student? not sure if we should implement it here
+
+
 }
--- src/Domain/Entities/Student.cs
+++ src/Domain/Entities/Student.cs
@@ -10,9 +10,62 @@ public class Student : BaseAuditableEntity
     public IList<Guardian> Guardians { get; private set; } = [];
     public Address Address { get; private set; } = null!;
 
-    // Factory Create
+    public static Student Create(
+        string firstName,
+        string lastName,
+        string email,
+        IList<Guardian> guardians,
+        Address address
+    )
+    {
+        var student = new Student();
+        student.SetDetails(firstName, lastName, email, guardians, address);
+        return student;
+    }
 
-    // Update
+    public void UpdateDetails(
+        string firstName,
+        string lastName,
+        string email,
+        IList<Guardian> guardians,
+        Address address
+    )
+    {
+        SetDetails(firstName, lastName, email, guardians, address);
+    }
+
+    private void SetDetails(
+        string firstName,
+        string lastName,
+        string email,
+        IList<Guardian> guardians,
+        Address address
+    )
+    {
+        ValidateFields(firstName, lastName, email, address, guardians);
+
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Guardians = guardians;
+        Address = address;
+    }
+
+    private static void ValidateFields(string firstName, string lastName, string email, Address address, IList<Guardian> guardians)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException("FirstName is required.", nameof(firstName));
+
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException("LastName is required.", nameof(lastName));
+
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("email is required.", nameof(email));
+
+        ArgumentNullException.ThrowIfNull(address);
+
+        ArgumentNullException.ThrowIfNull(guardians);
+    }
 
     // Soft Delete
 
