98261ac49527db3ecc5b2c519a80081825d1243d
Andrey-Sabile <sabileandrey@gmail.com>
2025-10-20 06:26:28 +0800

    feat: add students endpoints

---
+++ src/Web/Endpoints/Students.cs
@@ -0,0 +1,30 @@
+﻿using LeoLMS.Application.Students.Commands.CreateStudent;
+using LeoLMS.Application.Students.Commands.UpdateStudent;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace LeoLMS.Web.Endpoints;
+
+public class Students : EndpointGroupBase
+{
+    public override void Map(RouteGroupBuilder groupBuilder)
+    {
+        groupBuilder.MapPost(CreateStudent).RequireAuthorization();
+        groupBuilder.MapPut(UpdateStudent, "{id}").RequireAuthorization();
+    }
+
+    public async Task<Created<int>> CreateStudent(ISender sender, CreateStudentCommand command)
+    {
+        var id = await sender.Send(command);
+
+        return TypedResults.Created($"/{nameof(Students)}/{id}", id);
+    }
+
+    public async Task<Results<NoContent, BadRequest>> UpdateStudent(ISender sender, int id, UpdateStudentCommand command)
+    {
+        if (id != command.Id) return TypedResults.BadRequest();
+
+        await sender.Send(command);
+
+        return TypedResults.NoContent();
+    }
+}
