bbab7e56ecc2730a31d44b7d5576e112164888da
Andrey-Sabile <sabileandrey@gmail.com>
2025-10-20 18:44:08 +0800

    Removed unnecessary references

--- src/Web/ClientApp/src/app/core/layout/app-shell/app-shell.component.ts
+++ src/Web/ClientApp/src/app/core/layout/app-shell/app-shell.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavMenuComponent } from '@app/shared/ui/nav-menu/nav-menu.component';
 import { SideNavComponent } from '@app/shared/ui/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-shell',
   standalone: true,
   templateUrl: './app-shell.component.html',
-  imports: [NavMenuComponent, RouterOutlet, SideNavComponent]
+  imports: [RouterOutlet, SideNavComponent]
 })
 export class AppShellComponent { }
