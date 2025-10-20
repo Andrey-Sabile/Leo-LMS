816ffd8b3475647aa1955427c6dd140d3a103425
Andrey-Sabile <sabileandrey@gmail.com>
2025-10-20 10:08:35 +0800

    Scaffolded a shell for Contacts Component

--- src/Web/ClientApp/src/app/features/contacts/components/contacts/contacts.component.ts
+++ src/Web/ClientApp/src/app/features/contacts/components/contacts/contacts.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
+import { GuardiansClient, StudentsClient, StudentDto } from '@app/data-access/api/api-client';
 
 @Component({
   selector: 'app-contacts',
+  standalone: true,
   imports: [],
   templateUrl: './contacts.component.html',
-  styles: ``
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactsComponent {
+  private studentsClient = inject(StudentsClient);
+  readonly students = signal<StudentDto[]>([]);
 
+  private readonly refreshStudentsEffect = effect(
+    () => {
+      this.loadStudents();
+    },
+    { allowSignalWrites: true }
+  );
+
+  private loadStudents(): void {
+    this.studentsClient.getStudents()
+      .subscribe({
+        next: result => this.students.set(result.students),
+        error: error => console.error(error),
+      });
+  }
 }
