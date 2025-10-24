ea4e1a7a5b13640691d40a4de50b261a9ed05c36
Andrey-Sabile <sabileandrey@gmail.com>
2025-10-24 18:54:12 +0800

    Restyled sidebar

--- src/Web/ClientApp/src/app/core/layout/app-shell/app-shell.component.ts
+++ src/Web/ClientApp/src/app/core/layout/app-shell/app-shell.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SideNavComponent } from '@app/shared/ui/side-nav/side-nav.component';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { provideIcons, NgIcon } from '@ng-icons/core';
+import { heroHome, heroBookOpen, heroCalendar, heroListBullet, heroPhone } from '@ng-icons/heroicons/outline';
+
+type NavLink = {
+  label: string;
+  route: string;
+  exact?: boolean;
+  iconPaths: string;
+};
 
 @Component({
   selector: 'app-shell',
-
   templateUrl: './app-shell.component.html',
-  imports: [RouterOutlet, SideNavComponent]
+  viewProviders: [provideIcons({ heroHome, heroBookOpen, heroCalendar, heroListBullet, heroPhone })],
+  imports: [RouterOutlet, NgIcon, RouterLink, RouterLinkActive]
 })
-export class AppShellComponent { }
+export class AppShellComponent {
+  protected readonly navLinks: readonly NavLink[] = [
+    {
+      label: 'Home',
+      route: '/',
+      exact: true,
+      iconPaths: 'heroHome'
+    },
+    {
+      label: 'Classes',
+      route: '/classes',
+      iconPaths: 'heroBookOpen'
+
+    },
+    {
+      label: 'Calendar',
+      route: '/calendar',
+      exact: true,
+      iconPaths: 'heroCalendar'
+
+    },
+    {
+      label: 'Student Directory',
+      route: '/student-directory',
+      exact: true,
+      iconPaths: 'heroPhone'
+    },
+    {
+      label: 'Todo',
+      route: '/todo',
+      iconPaths: 'heroListBullet'
+    },
+
+  ];
+}
